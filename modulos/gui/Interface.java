0269562e88093f409d7911b319e9176432c12a1c
SauloPiccirilo <53665222+SauloPiccirilo@users.noreply.github.com>
2022-12-01 16:33:17 +0000

    ecrã princiapl Interface.Interface

--- modulos/gui/Interface.java
+++ modulos/gui/Interface.java
@@ -1,17 +1,28 @@
 package modulos.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Interface {
 
     public Interface () {
         JFrame framePrincipal = new JFrame("Bem-vindo ao StarThrive");
-        framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        framePrincipal.setSize(800, 600);
-        framePrincipal.setVisible(true);
+        JButton informacoesEmpresas= new JButton("Informacoes");
+
+        JButton alterarDados= new JButton("Alterar Dados");
 
         JPanel panelPrincipal = new JPanel();
-        framePrincipal.add(panelPrincipal);
+        panelPrincipal.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panelPrincipal.setLayout(new GridLayout(0,1));
+        panelPrincipal.add(informacoesEmpresas);
+        panelPrincipal.add(alterarDados);
+
+        framePrincipal.add(panelPrincipal, BorderLayout.CENTER);
+        framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        framePrincipal.setTitle("Ecrã Principal");
+        framePrincipal.pack();
+        framePrincipal.setVisible(true);
+
 
     }
 }
